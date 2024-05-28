ca647d865cde3d55e4b282c5f89f0a8950f798ce
unknown <dracokd19@gmail.com>
2024-05-28 21:19:27 +0545

    implemented logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useAppDispatch } from "../app/hooks";
+import { logout } from "../features/todo/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
   return (
     <>
       <div className="flex justify-between items-center p-4 bg-black text-white w-full absolute">
-        Name
+        <div>Todo</div>
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
       </div>
     </>
   );
